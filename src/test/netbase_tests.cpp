4f58e5a18edb1f742a1fe4f3dbd5dc4f01bebb5a
BITP <53627179+xiazhong123@users.noreply.github.com>
2019-09-03 12:30:16 +0800

    Add files via upload

--- src/test/netbase_tests.cpp
+++ src/test/netbase_tests.cpp
@@ -62,15 +62,15 @@ BOOST_AUTO_TEST_CASE(netbase_splithost)
     BOOST_CHECK(TestSplitHost("www.bitcoin.org:80", "www.bitcoin.org", 80));
     BOOST_CHECK(TestSplitHost("[www.bitcoin.org]:80", "www.bitcoin.org", 80));
     BOOST_CHECK(TestSplitHost("127.0.0.1", "127.0.0.1", -1));
-    BOOST_CHECK(TestSplitHost("127.0.0.1:11119", "127.0.0.1", 11119));
+    BOOST_CHECK(TestSplitHost("127.0.0.1:11120", "127.0.0.1", 11120));
     BOOST_CHECK(TestSplitHost("[127.0.0.1]", "127.0.0.1", -1));
-    BOOST_CHECK(TestSplitHost("[127.0.0.1]:11119", "127.0.0.1", 11119));
+    BOOST_CHECK(TestSplitHost("[127.0.0.1]:11120", "127.0.0.1", 11120));
     BOOST_CHECK(TestSplitHost("::ffff:127.0.0.1", "::ffff:127.0.0.1", -1));
-    BOOST_CHECK(TestSplitHost("[::ffff:127.0.0.1]:11119", "::ffff:127.0.0.1", 11119));
-    BOOST_CHECK(TestSplitHost("[::]:11119", "::", 11119));
-    BOOST_CHECK(TestSplitHost("::11119", "::11119", -1));
-    BOOST_CHECK(TestSplitHost(":11119", "", 11119));
-    BOOST_CHECK(TestSplitHost("[]:11119", "", 11119));
+    BOOST_CHECK(TestSplitHost("[::ffff:127.0.0.1]:11120", "::ffff:127.0.0.1", 11120));
+    BOOST_CHECK(TestSplitHost("[::]:11120", "::", 11120));
+    BOOST_CHECK(TestSplitHost("::11120", "::11120", -1));
+    BOOST_CHECK(TestSplitHost(":11120", "", 11120));
+    BOOST_CHECK(TestSplitHost("[]:11120", "", 11120));
     BOOST_CHECK(TestSplitHost("", "", -1));
 }
 
@@ -85,10 +85,10 @@ bool static TestParse(string src, string canon)
 BOOST_AUTO_TEST_CASE(netbase_lookupnumeric)
 {
     BOOST_CHECK(TestParse("127.0.0.1", "127.0.0.1:65535"));
-    BOOST_CHECK(TestParse("127.0.0.1:11119", "127.0.0.1:11119"));
+    BOOST_CHECK(TestParse("127.0.0.1:11120", "127.0.0.1:11120"));
     BOOST_CHECK(TestParse("::ffff:127.0.0.1", "127.0.0.1:65535"));
     BOOST_CHECK(TestParse("::", "[::]:65535"));
-    BOOST_CHECK(TestParse("[::]:11119", "[::]:11119"));
+    BOOST_CHECK(TestParse("[::]:11120", "[::]:11120"));
     BOOST_CHECK(TestParse("[127.0.0.1]", "127.0.0.1:65535"));
     BOOST_CHECK(TestParse(":::", ""));
 }
